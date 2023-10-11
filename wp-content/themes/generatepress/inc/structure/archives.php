f08c1f2df3f3d7d62490edc8923bdd162f9ba496
monikanabhoya <monika.nabhoya@gmail.com>
2023-10-11 15:34:11 +0530

    initialized project

--- wp-content/themes/generatepress/inc/structure/archives.php
+++ wp-content/themes/generatepress/inc/structure/archives.php
@@ -33,9 +33,9 @@ if ( ! function_exists( 'generate_archive_title' ) ) {
 			?>
 
             
-            <!--<h1 class="page-title">
+            <h1 class="page-title">
 				<?php //the_archive_title(); ?>
-			</h1>-->
+			</h1>
 
 			<?php
 			/**
